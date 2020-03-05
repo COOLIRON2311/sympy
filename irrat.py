2e2d0822eb081479850e0540789c44536973dbab
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2020-03-05 18:55:07 +0300

    Some minor improvements

--- irrat.py
+++ irrat.py
@@ -1,12 +1,12 @@
-from sympy import Add, Integral, Poly, Pow, pprint, sqrt, Symbol
+from sympy import Add, Integral, Poly, Pow, pprint, sqrt, Symbol, simplify, \
+     diff, solve
 from sympy.abc import x
 from sympy.core.numbers import Half
 
-import sympy as sp
 
-
-def euler(self: Integral) -> Add:
-    """Implements Euler substitution for irrational integrals"""
+def euler(*args, **kwargs) -> Add:
+    """Integrate irrational function using Euler substitution"""
+    integral = Integral(*args, **kwargs)
     _sqrt = []  # Искомый корень для замены
 
     def __sqrt_parse(functions_tuple):
@@ -18,36 +18,59 @@ def euler(self: Integral) -> Add:
             else:
                 __sqrt_parse(f.args)
 
-    __sqrt_parse(self.args)
+    __sqrt_parse(integral.args)
     if not _sqrt:  # Корень не найден
         raise Warning("Square root not found in expression")
-    _sqrt = _sqrt[0]
-    _coeffs = Poly(_sqrt**2).all_coeffs()  # Коэфиценты многочлена под корнем
-    assert(len(_coeffs) == 3)
-    _t = Symbol('t')   
+    _sqrt = _sqrt[0]                                              # 0  1  2
+    coeffs = Poly(_sqrt**2).all_coeffs()  # Коэфиценты многочлена: [a, b, c]
+    assert(len(coeffs) == 3)
+    x = integral.args[1][0]
+    t = Symbol('t')
 
-    if _coeffs[0] > 0:  # a > 0
-        _x = (_coeffs[2]-_t**2)/(2*sqrt(_coeffs[0])*_t-_coeffs[1])
-        _tx = _sqrt - sqrt(_coeffs[0])*x
-        _dx =  sp.simplify(sp.diff(_x,_t))
-        _sqrtt = sqrt(_coeffs[0])*_x+_t
-        return Integral(self.args[0].subs([(_sqrt, _sqrtt), (x,_x)])*_dx,(_t, self.args[1][1:])).doit().subs(_t, _tx)
-    elif _coeffs[2] > 0:  # c > 0
+    if coeffs[0] > 0:  # a > 0
+        _x = (coeffs[2] - t**2)/(2 * sqrt(coeffs[0])
+                                   * t - coeffs[1])  # x через t
+        tx = _sqrt - sqrt(coeffs[0]) * x  # t через x
+        dx = simplify(diff(_x, t))  # dx через t
+        sqrtt = sqrt(coeffs[0]) * _x + t  # корень через t
+    elif coeffs[2] > 0:  # c > 0
+        _x = None
+        tx = None
+        dx = None
+        sqrtt = None
         ...
     else:  # third case
-        ...
-    #return Integral(self.args[0].subs([(_sqrt, _sqrtt), (x,_x)])*_dx,
-                   #  (_t, self.args[1][1:])).doit().subs(_t, _tx)
-    return _sqrt  #TODO: Реализация подстановок
+        '''
+        discriminant = coeffs[1]**2 - 4 * coeffs[0] * coeffs[1]  # Дискриминант
+        if discriminant >= 0:  # TODO: Или через solve
+        '''
+        sols = solve(_sqrt**2)
+        if sols:
+            _x = None
+            tx = None
+            dx = None
+            sqrtt = None
+            ...
+        else:
+            raise Warning("Euler substitution is impossible. "
+                          "Try different integration methods.")
+    return Integral(simplify(integral.args[0].subs({_sqrt: sqrtt, x: _x}) *
+                    dx), (t, integral.args[1][1:])).doit().subs(t, tx)
 
 
-def diff_binomial(self: Integral) -> Add:
+def tan_ha(*args, **kwargs) -> Add:
+    """Integrate function using Tangent half-angle substitution"""
+    ...  # TODO: Реализация
+
+
+def diff_binomial(*args, **kwargs) -> Add:
+    """Integrate function as Differential binomial"""
     ...  # TODO: Реализация
 
 
 Integral.euler = euler
 
-a = Integral(x*sqrt(x**2-2*x+2), x)
-pprint(a)
+a = Integral(x*sqrt(x**2 - 2*x + 2), x)
+# pprint(a, use_unicode=False)
 print()
-pprint(sp.simplify((a.euler())), use_unicode=False)
+pprint(a.euler(), use_unicode=False)
