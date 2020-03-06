b0ba49611907711883927864497a9d9548a4e193
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2020-03-06 19:20:02 +0300

    Tangent half-angle implementation

--- irrat.py
+++ irrat.py
@@ -1,5 +1,5 @@
 from sympy import Add, Integral, Poly, Pow, pprint, sqrt, Symbol, simplify, \
-     diff, solve
+     diff, solve, sin, cos, tan, cot
 from sympy.abc import x
 from sympy.core.numbers import Half
 
@@ -25,37 +25,47 @@ def euler(*args, **kwargs) -> Add:
     coeffs = Poly(_sqrt**2).all_coeffs()  # Коэфиценты многочлена: [a, b, c]
     assert(len(coeffs) == 3)
     x = integral.args[1][0]
-    t = Symbol('t')
+    _t = Symbol('t')
 
     if coeffs[0] > 0:  # a > 0
-        _x = (coeffs[2] - t**2)/(2 * sqrt(coeffs[0])
-                                   * t - coeffs[1])  # x через t
+        _x = (coeffs[2] - _t**2)/(2 * sqrt(coeffs[0])
+                                    * _t - coeffs[1])  # x через t
         tx = _sqrt - sqrt(coeffs[0]) * x  # t через x
-        dx = simplify(diff(_x, t))  # dx через t
-        sqrtt = sqrt(coeffs[0]) * _x + t  # корень через t
+        dx = simplify(diff(_x, _t))  # dx через t
+        sqrtt = sqrt(coeffs[0]) * _x + _t  # корень через t
     elif coeffs[2] > 0:  # c > 0
-        _x = (coeffs[1]-2*sqrt(coeffs[2])*t)/(t**2-coeffs[0])  # x через t
+        _x = (coeffs[1]-2*sqrt(coeffs[2])*_t)/(_t**2 - coeffs[0])  # x через t
         tx = (_sqrt-sqrt(coeffs[2]))/x   # t через x
-        dx = simplify(diff(_x, t))  # dx через t
-        sqrtt = _x*t + sqrt(coeffs[2])  # корень через t
+        dx = simplify(diff(_x, _t))  # dx через t
+        sqrtt = _x*_t + sqrt(coeffs[2])  # корень через t
     else:  # third case   # sqrt(a*(x-x1)*(x-x2))
         sols = solve(_sqrt**2)
         if len(sols) == 2:
-            _x = (t**2 * sols[0] - coeffs[0] *
-                  sols[1])/(t**2 - coeffs[0])  # x через t
+            _x = (_t**2 * sols[0] - coeffs[0] *
+                  sols[1])/(_t**2 - coeffs[0])  # x через t
             tx = _sqrt/(x - sols[0])  # t через x
-            dx = simplify(diff(_x, t))  # dx через t
-            sqrtt = t*(_x - sols[0])  # корень через t
+            dx = simplify(diff(_x, _t))  # dx через t
+            sqrtt = _t*(_x - sols[0])  # корень через t
         else:
             raise Warning("Euler substitution is impossible. "
                           "Try different integration methods.")
     return Integral(simplify(integral.args[0].subs({_sqrt: sqrtt, x: _x}) *
-                    dx), (t, integral.args[1][1:])).doit().subs(t, tx)
+                    dx), (_t, integral.args[1][1:])).doit().subs(_t, tx)
 
 
 def tan_ha(*args, **kwargs) -> Add:
     """Integrate function using Tangent half-angle substitution"""
-    ...  # TODO: Реализация
+    x = args[1][0]
+    _t = Symbol('t')
+    _sin = (2 * _t)/(1 + _t**2)
+    _cos = (1 - _t**2)/(1 + _t**2)
+    _tg = _sin/_cos
+    _ctg = 1/_tg
+    _dx = 2/(1 + _t**2)
+    _f = simplify(args[0].subs({sin(x): _sin, cos(x): _cos, tan(x): _tg,
+                                cot(x): _ctg}) * _dx)
+    return simplify(Integral(_f, (_t, *args[1][1:]),
+                             **kwargs).doit().subs(_t, tan(x/2)))
 
 
 def diff_binomial(*args, **kwargs) -> Add:
@@ -65,7 +75,10 @@ def diff_binomial(*args, **kwargs) -> Add:
 
 Integral.euler = euler
 
-a = Integral(1/(1 + sqrt(-1 + 50*x - x**2)), x)
-# pprint(a, use_unicode=False)
+# a = Integral(1/(2*sin(x) - cos(x) + 5), x)
+a = Integral((sin(x)**2/(1 + sin(x)**2)), x)
+# a = Integral(1/(1 + sqrt(-1 + 50*x - x**2)), x)
+pprint(a, use_unicode=False)
 print()
-pprint(a.euler(), use_unicode=False)
+pprint(simplify(tan_ha(*a.args)))
+# pprint(a.euler(), use_unicode=False)
