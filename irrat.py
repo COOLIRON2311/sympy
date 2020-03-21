14d373c89cdfb44e023b7c843b2fa7abbe88d060
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2020-03-21 22:29:05 +0300

    Some improvements to the code

--- irrat.py
+++ irrat.py
@@ -1,15 +1,16 @@
 from sympy import Add, Integral, Poly, Pow, pprint, sqrt, Symbol, simplify, \
-     diff, solve, sin, cos, tan, cot
+     diff, solve, sin, cos, tan, cot, Tuple
 from sympy.abc import x
 from sympy.core.numbers import Half
 
 
-def euler(*args, **kwargs) -> Add:
+def euler(f, d) -> Add:
     """Integrate irrational function using Euler substitution"""
-    integral = Integral(*args, **kwargs)
+    if type(d) not in (Tuple, tuple):
+        d = (d,)
     _sqrt = []  # Искомый корень для замены
 
-    def __sqrt_parse(functions_tuple):
+    def __sqrt_parse(functions_tuple: tuple) -> None:
         for f in functions_tuple:
             if type(f) is Pow and type(f.args[0]) is Add \
                     and type(f.args[1]) is Half:
@@ -19,13 +20,12 @@ def euler(*args, **kwargs) -> Add:
             else:
                 __sqrt_parse(f.args)
 
-    __sqrt_parse(integral.args)
-    if len(_sqrt) != 1:  # Корень не найден или их несколько
-        raise Warning("None or more than one square roots found in expression")
+    __sqrt_parse(f.args)
+    # Корень не найден или их несколько
+    assert len(_sqrt) == 1, f'One square root expected, found {len(_sqrt)}'
     _sqrt = _sqrt[0]                                              # 0  1  2
     coeffs = Poly(_sqrt**2).all_coeffs()  # Коэфиценты многочлена: [a, b, c]
-    assert(len(coeffs) == 3)
-    x = integral.args[1][0]
+    assert len(coeffs) == 3, f'{_sqrt**2} is not a square trinomial'
     _t = Symbol('t')
 
     if coeffs[0] > 0:  # a > 0
@@ -49,34 +49,33 @@ def euler(*args, **kwargs) -> Add:
             dx = simplify(diff(_x, _t))  # dx через t
             sqrtt = _t*(_x - sols[0])  # корень через t
         else:
-            raise Warning("Euler substitution is impossible. "
-                          "Try different integration methods.")
-    return Integral(simplify(integral.args[0].subs({_sqrt: sqrtt, x: _x})*dx),
-                    (_t, integral.args[1][1:]), **kwargs).doit().subs(_t, tx)
+            raise Warning('Euler substitution is impossible. '
+                          'Try different integration methods.')
+    return Integral(simplify(f.subs({_sqrt: sqrtt, d[0]: _x})*dx),
+                    (_t, d[1:])).doit().subs(_t, tx)
 
 
-def tan_ha(*args, **kwargs) -> Add:
+def tan_ha(f, d) -> Add:
     """Integrate function using Tangent half-angle substitution"""
-    integral = Integral(*args, **kwargs)
-    x = integral.args[1][0]
+    if type(d) not in (Tuple, tuple):
+        d = (d,)
     _t = Symbol('t')
     _sin = (2 * _t)/(1 + _t**2)
     _cos = (1 - _t**2)/(1 + _t**2)
     _tg = _sin/_cos
     _ctg = 1/_tg
     _dx = 2/(1 + _t**2)
-    _f = simplify(args[0].subs({sin(x): _sin, cos(x): _cos, tan(x): _tg,
-                                cot(x): _ctg}) * _dx)
-    return simplify(Integral(_f, (_t, integral.args[1][1:]),
-                             **kwargs).doit().subs(_t, tan(x/2)))
+    _f = simplify(f.subs({sin(x): _sin, cos(x): _cos, tan(x): _tg,
+                          cot(x): _ctg}) * _dx)
+    return simplify(Integral(_f, (_t, d[1:])).doit().subs(_t, tan(x/2)))
 
 
-def diff_binomial(*args, **kwargs) -> Add:
+def diff_binomial(f, d) -> Add:
     """Integrate function as Differential binomial"""
+    if type(d) not in (Tuple, tuple):
+        d = (d,)
     ...  # TODO: Реализация
 
-
-Integral.euler = euler
 
 # a = Integral(1/(2*sin(x) - cos(x) + 5), x)
 # a = Integral((sin(x)**2/(1 + sin(x)**2)), x)
@@ -85,4 +84,4 @@ a = Integral(1/(x + sqrt(x**2 + x + 1)), x)
 # pprint(a, use_unicode=False)
 print()
 # pprint(tan_ha(*a.args))
-pprint(a.euler(), use_unicode=False)
+pprint(euler(*a.args), use_unicode=False)
